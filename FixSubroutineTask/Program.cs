c692d26bd6adb1fdbe0f93f0d3de505a78dfed92
Ruby Abson <21RAbson@heckgrammar.co.uk>
2025-03-22 12:25:07 +0000

    homework

--- FixSubroutineTask/Program.cs
+++ FixSubroutineTask/Program.cs
@@ -4,15 +4,15 @@
     {
         static void Main(string[] args)
         {
-            for(int i = 8;i >= 0; i++) {
+            for(int i = 0;i <= 7; i++) 
+            {
                 Console.WriteLine(diffCurrencies(i));            
             }
         }
-        static string diffCurrencies(int x)
+        static string diffCurrencies(int i)
         {
             string[] currencies = { "baht", "dollar", "euro", "koruna", "lira", "rand", "rupee", "yen" };
-            return currencies[1];
-
+            return currencies[i];
         }
     }
 }
